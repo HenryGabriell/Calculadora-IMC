82b547fd0d2b1a8a7a1c97e9b7ba97bc6b3fdc24
Henry Gabriel <96191361+HenryGabriell@users.noreply.github.com>
2022-09-05 18:26:33 -0300

    graus dos pesos

--- imc.js
+++ imc.js
@@ -11,7 +11,23 @@ function imc(){
     if(nome !== '' && altura !== '' && peso !== ''){
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
-        resultado.textContent = valorIMC;
+        let classificacao = "";
+
+        if (valorIMC < 18.5){
+            classificacao= "Abaixo do Peso !";
+        } else if (valorIMC < 24.9){
+            classificacao= "Peso Ideal, Parabéns !!";
+        } else if(valorIMC < 29.9){
+            classificacao= "Levemente acima do Peso !";
+        } else if(valorIMC < 34.9){
+            classificacao= "Obesidade grau 1 !";
+        } else if(valorIMC < 39.9){
+            classificacao= "Obesidade grau 2 (severa)!";
+        } else if(valorIMC > 40){
+            classificacao= "Obesidade grau 3 (CUIDADO)!";
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você esta ${classificacao}`;
     } else{
         resultado.textContent = 'Preencha todos os campos!'
     }
