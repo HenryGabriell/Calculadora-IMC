78258f5f66fea3be68bd925f8555b310e2933e61
Henry Gabriel <96191361+HenryGabriell@users.noreply.github.com>
2022-09-04 23:07:50 -0300

    testes com alerta

--- imc.js
+++ imc.js
@@ -2,10 +2,17 @@
 const calcular = document.getElementById('calcular');
 
 function imc(){
-    const nome = document.getElementById('nome');
-    const altura = document.getElementById('altura');
-    const peso =  document.getElementById('peso');
-    const resultado = document.getElementById('resultado')
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso =  document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    //Se o a const for Diferente de Vazio 
+    if(nome !== '' && altura !== '' && peso !== ''){
+        alert ('valor preenchido')
+    } else{
+        alert('Preencha os Campos')
+    }
 }
 
-calcular.addEventListener('click', imc);
+calcular.addEventListener('click', imc); 
